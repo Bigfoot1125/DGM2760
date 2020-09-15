e8feac2e19e6e6bb492de5058782adf30b1609fd
Bigfoot1125 <colthedin@gmail.com>
2020-09-15 14:56:53 -0600

    first commit

---
+++ 01-JoesBB/js/main.js
@@ -0,0 +1,5 @@
+//document.getElementById('company').innerHTML = "Joe's Bed and Breakfast"
+
+document.querySelector('#company').innerText = "Joes Bed and Breakfast"
+
+document.querySelector('header > h2').innerText = "Best B&B This side of Logan"
