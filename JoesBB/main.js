35297c588904cf4958833b96fae7b184bd36e8d4
Bigfoot1125 <colthedin@gmail.com>
2020-09-22 16:42:07 -0600

    added a prompt

--- JoesBB/main.js
+++ JoesBB/main.js
@@ -3,3 +3,10 @@
 document.querySelector('#company').innerText = "Joes Bed and Breakfast"
  
 document.querySelector('header > h2').innerText = "Best B&B This side of Logan"
+
+
+let userName = prompt("what is your name?")
+
+let message = "Hello" + userName + ", welcom to the best lodging in Utah"
+
+document.querySelector('#greeting').innerText = message
